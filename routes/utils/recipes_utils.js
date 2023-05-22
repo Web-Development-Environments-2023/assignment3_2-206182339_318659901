4da2797721bbdeb2687a3487f28ff4eb7d778a7b
MichalAlhindi <michalalhindi@gmail.com>
2023-05-22 18:30:35 +0300

    first

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -5,7 +5,7 @@ const api_domain = "https://api.spoonacular.com/recipes";
 
 /**
  * Get recipes list from spooncular response and extract the relevant recipe data for preview
- * @param {*} recipes_info 
+ * @param {*}  recipes_info
  */
 
 
@@ -13,12 +13,50 @@ async function getRecipeInformation(recipe_id) {
     return await axios.get(`${api_domain}/${recipe_id}/information`, {
         params: {
             includeNutrition: false,
-            apiKey: process.env.spooncular_apiKey
+            apiKey: process.env.api_token
         }
     });
 }
 
-
+function extractPreviewRecipeDetails(recipes_info) {
+    return recipes_info.map((recipe_info) => {
+        //check the data type so it can work with diffrent types of data
+        let data = recipe_info;
+        if (recipe_info.data) {
+            data = recipe_info.data;
+        }
+        const {
+            id,
+            title,
+            readyInMinutes,
+            image,
+            aggregateLikes,
+            vegan,
+            vegetarian,
+            glutenFree,
+        } = data;
+        return {
+            id: id,
+            title: title,
+            image: image,
+            readyInMinutes: readyInMinutes,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree
+        }
+    })
+  }
+  async function getRecipesPreview(recipes_ids_list) {
+    let promises = [];
+    recipes_ids_list.map((id) => {
+        promises.push(getRecipeInformation(id));
+    });
+    let info_res = await Promise.all(promises);
+    info_res.map((recp)=>{console.log(recp.data)});
+    // console.log(info_res);
+    return extractPreviewRecipeDetails(info_res);
+  }
 
 async function getRecipeDetails(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
@@ -41,5 +79,5 @@ async function getRecipeDetails(recipe_id) {
 
 exports.getRecipeDetails = getRecipeDetails;
 
-
+exports.getRecipesPreview = getRecipesPreview;
 
